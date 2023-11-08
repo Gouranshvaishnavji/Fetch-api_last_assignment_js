5aacdbe7372b89bfbe69338c54719122ccc44482
Gouranshvaishnavji <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-11-08 14:14:11 +0530

    Initial commit

---
+++ weather.js
@@ -0,0 +1,5 @@
+//Complete the Weather API Backend part using openweathermap api
+
+// Progression 1: Create a function and fetch data using "fetch" from openweathermap api and display the data as given in reference image.
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1342f8dd595eda677ca40143519800de').then((data) => data.json()).then((parsedData) => console.log(parsedData));
